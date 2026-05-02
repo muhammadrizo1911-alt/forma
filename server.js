e6137b535b9307e762507e8acbff7e328feabe16
Muhammadrizo <muhammadrizo1911@gmail.com>
2026-05-02 14:31:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const dbConfig = {
   host: process.env.MYSQLHOST || 'localhost',
   port: process.env.MYSQLPORT || 3306,
   user: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || '',
+  password: process.env.MYSQLPASSWORD || 'obNOzTnpcLlfGyhZmkeZZgVXZQEvNmtl',
   database: process.env.MYSQLDATABASE || 'railway',
 };
 
